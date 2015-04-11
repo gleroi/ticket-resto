ffa92a0229a76b27c576a0692fee13d3c5cc88e5
Guillaume Leroi <leroi.g@gmail.com>
2015-04-11 11:50:20 +0200

    auto asset generation from svg

---
+++ TicketResto.PhoneApp/Assets/build-asset.ps1
@@ -0,0 +1,4 @@
+﻿inkscape --export-png=Logo.scale-240.png --export-area-page --export-height=360 --export-width=360 .\logo.svg
+inkscape --export-png=SmallLogo.scale-240.png --export-area-page --export-height=106 --export-width=106 .\logo.svg
+inkscape --export-png=Square71x71Logo.scale-240.png --export-area-page --export-height=170 --export-width=170 .\logo.svg
+inkscape --export-png=StoreLogo.scale-240.png --export-area-page --export-height=120 --export-width=120 .\logo.svg
